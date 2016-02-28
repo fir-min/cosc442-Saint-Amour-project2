a7102909670347511d90c6fa21ed3c9f78e61bc0
Firmin Saint-Amour <firminsa@gmail.com>
2016-02-27 21:41:39 -0500

    Working on the vending Machine
    Started task 4

---
+++ VendingMachine/src/edu/towson/cis/cosc442/project2/vendingmachine/VendingMachineItemTest.java
@@ -0,0 +1,29 @@
+package edu.towson.cis.cosc442.project2.vendingmachine;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class VendingMachineItemTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGetName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPrice() {
+		fail("Not yet implemented");
+	}
+
+}
